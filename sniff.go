f2e601a5595fb00d89b28ac5f7d3c8edd5515681
Arthur Arnx <arthur.arnx@gmail.com>
2023-02-18 22:56:47 +0100

    use bytes array for addr

--- sniff.go
+++ sniff.go
@@ -5,9 +5,47 @@ import (
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"sync"
+	"net"
 	//"time"
-	//"strings"
+	"strings"
+	"strconv"
 )
+
+var (
+	localIP = GetOutboundIP()
+)
+
+func GetOutboundIP() []byte {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        return []byte{0,0,0,0}
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+	octets := strings.Split(localAddr.IP.String(), ".")
+
+	octet0, _ := strconv.Atoi(octets[0])
+	octet1, _ := strconv.Atoi(octets[1])
+	octet2, _ := strconv.Atoi(octets[2])
+	octet3, _ := strconv.Atoi(octets[3])
+
+	b := []byte{byte(octet0),byte(octet1),byte(octet2),byte(octet3)}
+
+    return b
+}
+
+func adressSlicesEqual(addr1 []byte, addr2 []byte) bool {
+	if len(addr1) != len(addr2) {
+        return false
+    }
+    for i := range addr1 {
+        if addr1[i] != addr2[i] {
+            return false
+        }
+    }
+    return true
+}
 
 func listenDevice(device pcap.Interface) {
 	fmt.Printf("Device Name: %s\n", device.Name)
@@ -23,12 +61,17 @@ func listenDevice(device pcap.Interface) {
 	
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		if packet.NetworkLayer() != nil { // ARP packets do not have Network layer
+		if packet.NetworkLayer() != nil { // for example ARP packets do not have Network layer
 			netFlow := packet.NetworkLayer().NetworkFlow()
 			src, dst := netFlow.Endpoints()
-			fmt.Println(src.String() + " -> " + dst.String())
+			fmt.Println(device.Name + " : " + src.String() + " -> " + dst.String())
 			if dst.String() == "239.255.255.250" {
 				 fmt.Println("SSDP")
+			}
+			if adressSlicesEqual(dst.Raw(), localIP) {
+				// in
+			} else if adressSlicesEqual(src.Raw(), localIP) {
+				// out
 			}
 			// } else if dst.String() == "127.0.0.1" {
 			// 	fmt.Println("in")
